82a5e6b922abc55396dbedfbe88262a60811914e
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2020-10-06 11:09:02 +0200

    Beispiel 4

---
+++ Beispiel4/src/pos3/tsturm18/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos3.tsturm18;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author timst
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int ergebniss = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(i -> i % 2 == 1).reduce(0, (b1, b2) -> b1 + b2);
+        System.out.println(ergebniss);
+    }
+
+}
