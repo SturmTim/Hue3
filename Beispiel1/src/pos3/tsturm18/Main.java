87567a83dfa79a562e29c68e3d3bfd8d585ee296
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2020-10-06 11:47:55 +0200

    sortList + readWeapons

--- Beispiel1/src/pos3/tsturm18/Main.java
+++ Beispiel1/src/pos3/tsturm18/Main.java
@@ -5,6 +5,15 @@
  */
 package pos3.tsturm18;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author timst
@@ -14,8 +23,26 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    List<Weapon> list = new LinkedList<>();
+
     public static void main(String[] args) {
 
+    }
+
+    public void sortList() {
+        list.sort((Weapon o1, Weapon o2) -> Integer.compare(o1.getDamage(), o2.getDamage()));
+    }
+
+    public void readWeapons() {
+        try {
+            list = Files.lines(new File("weapons.csv")
+                    .toPath())
+                    .skip(1)
+                    .map(s -> s.split(";"))
+                    .map(s -> new Weapon(s[0], CombatType.valueOf(s[1]), DamageType.valueOf(s[2]), Integer.parseInt(s[3]), Integer.parseInt(s[4]), Integer.parseInt(s[5]), Integer.parseInt(s[6]))).collect(Collectors.toList());
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
